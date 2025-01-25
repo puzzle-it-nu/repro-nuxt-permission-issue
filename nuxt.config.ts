69ccaa086e4551c051a480b8a36da3abb06e141b
Joop van de Wege <joop@gbor.nl>
2025-01-25 20:44:57 +0100

    Using Daniels suggestion from #30459

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,5 +1,24 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
   compatibilityDate: '2024-11-01',
-  devtools: { enabled: true }
+  devtools: { enabled: true },
+  watchers: {
+    chokidar: {
+      ignorePermissionErrors: true,
+    },
+  },
+  vite: {
+    build: {
+      watch: {
+        chokidar: {
+          ignorePermissionErrors: true,
+        },
+      },
+    },
+    server: {
+      watch: {
+        ignorePermissionErrors: true,
+      },
+    },
+  },
 })
